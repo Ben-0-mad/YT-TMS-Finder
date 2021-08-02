f1722fdfb0b8a7341417afc806e4bf526b479d76
Tamago-iku <katsunejiku69@outlook.com>
2021-08-02 13:17:13 +1000

    changed logo in find_stable.py

--- TMS Finder my settings.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set Location=C:%HOMEPATH%\AppData\Local\Programs\Python\
-
-:Check to see if the Python directory exists, if not, display error message:
-IF EXIST "%Location%" ( 
-    python find_stable.py -m 3
-) ELSE (
-    echo This program uses Python, which is not installed on your system. Please download and install Python at https://www.python.org/downloads/. & pause
-)
